dbba09976adf57501811f8f3701fc1bafc69732a
yupshion <volcarona9227@gmail.com>
2016-02-20 14:42:07 +0900

    ちょっと進めた

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict';
+var
+    express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    parser = require('body-parser');
+
+
+app.use(parser.urlencoded({ extended:false }))   //追加
+app.use(parser.json());
+app.use(express.static(__dirname + '/public'))
+
+    app.get('/api/ping', function (req, res, next) {
+	    res.json('PONG');
+	    return next();
+	});
+
+// 追加
+  app.get('/api/projects', function (req, res, next) {
+    res.json('200 OK');
+    return next();
+});
+
+// 追加
+app.get('/api/project:id', function (req, res, next) {
+  var id = req.params.id;
+  res.json(id);
+	return next();
+    });
+
+app.get('/api/notfound', function (req, res, next) {
+  res.status(404).json('NotFound');
+  return next();
+    });
+
+
+
+app.post('/api/badrequest', function (req, res, next) {
+	res.status(400).json('BadRequest');
+	return next();
+    })
+
+    app.listen(port, function () {
+	    console.log('Server running with port', port);
+	});
